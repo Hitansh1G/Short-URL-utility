14bff5ecf9dac9b46f46263862db4f73cfcf4f9b
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-07-26 20:02:30 +0530

    100% completed

---
+++ x-url/src/test/java/com/crio/xurl/XUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crio.xurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
